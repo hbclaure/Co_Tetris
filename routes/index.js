8e2483488fe9420af2da5de84488e60c9937963b
Houston Claure <houstonclaure@Houstons-MacBook-Pro.local>
2024-04-24 22:11:57 -0400

    Minor Fixes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET the home page. */
 router.get('/', function (req, res, next) {
   res.sendFile('./public/index.html', function (err) {
     if (err) {
